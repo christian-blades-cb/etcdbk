70090a2539c9a2354006238ea0ce1b506561a0cc
Christian <christian.blades@careerbuilder.com>
2015-08-11 16:42:51 -0400

    moved the cluster name option in the s3 command

--- main.go
+++ main.go
@@ -8,7 +8,6 @@ import (
 
 var opts struct {
 	EtcdMachines []string `long:"etcd-hosts" short:"e" required:"true" default:"http://127.0.0.1:4001" env:"ETCD_HOSTS" env-delim:"," description:"etcd machines"`
-	ClusterName  string   `long:"cluster-name" short:"n" default:"etcd-cluster" env:"CLUSTER_NAME" description:"Cluster name to use in naming the file in the S3 Bucket"`
 	Verbose      func()   `long:"debug" short:"v" description:"verbose logging"`
 }
 
