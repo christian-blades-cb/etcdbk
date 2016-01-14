339d2a848113335fea0dc2352238f034784af7c0
Joshua Kwan <joshk@triplehelix.org>
2016-01-14 11:53:05 -0500

    etcdbk tar tweaks
    - Fix issue with tar file not being fully written out.
    
    - Write tar entries for empty dirs, erasing one of the listed caveats
    
    - Write dirs with good permissions so that the files can be read out
      after extracting with a normal tar app.
    
    - Write log output to stderr, so that it doesn't interfere with using
      etcdbk in a shell pipeline.
    
    - Factor out nodeExpiration and nodeXattrs into helper functions.

--- tarball.go
+++ tarball.go
@@ -14,12 +14,13 @@ func FillTarballBuffer(rootNode *etcd.Node) *bytes.Buffer {
 	buffer := bytes.NewBuffer(nil)
 
 	gzipWriter := gzip.NewWriter(buffer)
-	w := tar.NewWriter(gzipWriter)
+	tarWriter := tar.NewWriter(gzipWriter)
 
-	defer w.Close()
+	// LIFO order; close the tar writer, then the gzip writer.
 	defer gzipWriter.Close()
+	defer tarWriter.Close()
 
-	writeNode(w, rootNode)
+	writeNode(tarWriter, rootNode)
 
 	return buffer
 }
@@ -27,6 +28,16 @@ func FillTarballBuffer(rootNode *etcd.Node) *bytes.Buffer {
 func writeNode(w *tar.Writer, node *etcd.Node) { // I'm recursive!
 	log.WithField("key", node.Key).Debug("writing to tarball")
 	if node.Dir {
+		// Always write a header for a directory, unless it's the root.
+		if len(node.Key) > 0 {
+			w.WriteHeader(&tar.Header{
+				// Always strip the leading slash from the key.
+				Name:   node.Key[1:] + "/",
+				Mode:   0755,
+				Xattrs: nodeXattrs(node),
+			})
+		}
+
 		for _, subNode := range node.Nodes {
 			writeNode(w, subNode) // see?
 		}
@@ -34,23 +45,28 @@ func writeNode(w *tar.Writer, node *etcd.Node) { // I'm recursive!
 	}
 
 	buf := bytes.NewBuffer([]byte(node.Value))
-	expiration := func() string {
-		if node.Expiration == nil {
-			return "never"
-		} else {
-			return node.Expiration.Format(time.RFC3339)
-		}
-	}()
-
 	w.WriteHeader(&tar.Header{
-		Name: node.Key,
-		Mode: 0444,
-		Size: int64(buf.Len()),
-		Xattrs: map[string]string{
-			"ModifiedIndex": fmt.Sprintf("%d", node.ModifiedIndex),
-			"CreatedIndex":  fmt.Sprintf("%d", node.CreatedIndex),
-			"Expiration":    expiration,
-		},
+		// Always strip the leading slash from the key.
+		Name:   node.Key[1:],
+		Mode:   0644,
+		Size:   int64(buf.Len()),
+		Xattrs: nodeXattrs(node),
 	})
 	buf.WriteTo(w)
+}
+
+func nodeExpiration(node *etcd.Node) string {
+	if node.Expiration == nil {
+		return "never"
+	} else {
+		return node.Expiration.Format(time.RFC3339)
+	}
+}
+
+func nodeXattrs(node *etcd.Node) map[string]string {
+	return map[string]string{
+		"ModifiedIndex": fmt.Sprintf("%d", node.ModifiedIndex),
+		"CreatedIndex":  fmt.Sprintf("%d", node.CreatedIndex),
+		"Expiration":    nodeExpiration(node),
+	}
 }
